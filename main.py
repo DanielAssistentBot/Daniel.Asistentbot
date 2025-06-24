0c865c758013b48ea4a82417ec5d92ac53779a4e
DanielAssistentBot <danil.klyuchnikov@gmail.com>
2025-06-25 00:22:17 +0300

    main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
-TOKEN= 7832991702:AAFuyiAe9_twJk_MncmRNo2mU3O6qTpEWQM
+TOKEN = "7832991702:AAFuyiAe9_twJk_MncmRNo2mU3O6qTpEWQM"
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Привет! Я твой ассистент.")
